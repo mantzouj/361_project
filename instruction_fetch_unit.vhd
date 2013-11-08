b3075e9bb7ee5c329376e413917c4b64ee58f9b4
ConorHetland <conorhetland@gmail.com>
2013-11-08 15:41:47 -0600

    Update instruction_fetch_unit.vhd

--- instruction_fetch_unit.vhd
+++ instruction_fetch_unit.vhd
@@ -8,15 +8,19 @@ entity instruction_fetch_unit is
     clk : in std_logic;
     immediate : in std_logic_vector(15 downto 0);
     target : in std_logic_vector(25 downto 0);
-    branch : in std_logic;
+    brancheq : in std_logic;
+    branchneq: in std_logic;
     zero : in std_logic;
     instrOut : out std_logic_vector(31 downto 0)
   );
 end instruction_fetch_unit;
 
 architecture structural of instruction_fetch_unit is
+   signal zero_not: std_logic;
    signal ext_imm : std_logic_vector(31 downto 0);
-   signal b_and_z : std_logic;
+   signal be_and_z : std_logic;
+   signal bne_and_z : std_logic;
+   signal branch : std_logic;
    signal pc : std_logic_vector(29 downto 0);
    signal pc_plus_4 : std_logic_vector(29 downto 0);
    signal pc_plus_imm : std_logic_vector(29 downto 0);
@@ -32,12 +36,15 @@ begin
    
    extend : entity work.extender_16 port map('1', immediate, ext_imm);
    
-   bchzro: and_gate port map(branch, zero, b_and_z);
+   z_not: not_gate port map(zero, zero_not);
+   bcheq:  and_gate port map(brancheq, zero, be_and_z);
+   bchneq: and_gate port map(branchneq, zero_not, bne_and_z);
+   bch_or: or_gate port map(brancheq, branchneq, branch);
    
    pc_4 : fulladder_n generic map(30) port map('1', pc, "000000000000000000000000000000", c_out0,pc_plus_4);
    pc_imm: fulladder_n generic map(30) port map('0', pc_plus_4, ext_imm(29 downto 0), c_out1, pc_plus_imm); --I don't *think* there are any overflow worries for these adders
    
-   mux_0 : mux_n generic map(30) port map(b_and_z, pc_plus_4, pc_plus_imm, pc_next);
+   mux_0 : mux_n generic map(30) port map(branch, pc_plus_4, pc_plus_imm, pc_next);
    
    pc_reg: entity work.register_30 port map(pc_next, clk, '1', pc);
    
