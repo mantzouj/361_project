6087026cc71ff40a03b0346ea3c3006f31860716
ConorHetland <conorhetland@gmail.com>
2013-11-08 17:49:45 -0600

    Update register_32.vhd

--- register_32.vhd
+++ register_32.vhd
@@ -19,43 +19,34 @@ entity register_32 is
   
   component dff is
       port (
-	     clk	: in  std_logic;
-	     d	: in  std_logic;
-	     q	: out std_logic
+         clk        : in std_logic;
+         d        : in std_logic;
+         q        : out std_logic
       );
   end component dff;
     
   signal tempout : std_logic_vector(31 downto 0);
-  signal prein1 : std_logic_vector(31 downto 0);
-  signal prein2 : std_logic_vector(31 downto 0);
   signal tempin : std_logic_vector(31 downto 0);
-  signal notenable : std_logic;  
-  
-  
-  
     
   begin
-    not0 : not_gate port map (enable, notenable);
     
-  --  and0a : and_gate port map (tempout(0), notenable, prein1(0));
-  --  and0b : and_gate port map (Din(0),enable,prein2(0));
-  --  or0 : or_gate port map (prein1(0),prein2(0),tempin(0));
-  --  dff0 : dff port map (clk, tempin(0), tempout(0));
+  --and0a : and_gate port map (tempout(0), notenable, prein1(0));
+  --and0b : and_gate port map (Din(0),enable,prein2(0));
+  --or0 : or_gate port map (prein1(0),prein2(0),tempin(0));
+  --dff0 : dff port map (clk, tempin(0), tempout(0));
     
-  --  and1a : and_gate port map (tempout(1), notenable, prein1(1));
-  --  and1b : and_gate port map (Din(1),enable,prein2(1));
-  --  or1 : or_gate port map (prein1(1),prein2(1),tempin(1));
-  --  dff1 : dff port map (clk, tempin(1), tempout(1));     
-      
-    GEN_dff:
-      for i in 0 to 31 generate
-        andaX : and_gate port map (tempout(i),notenable,prein1(0));
-        andbX : and_gate port map (Din(i),enable,prein2(i));
-        orX : or_gate port map (clk, tempin(i), tempout(i));      
+  --and1a : and_gate port map (tempout(1), notenable, prein1(1));
+  --and1b : and_gate port map (Din(1),enable,prein2(1));
+  --or1 : or_gate port map (prein1(1),prein2(1),tempin(1));
+  --dff1 : dff port map (clk, tempin(1), tempout(1));
+    
+  GEN_dff:
+    for i in 0 to 31 generate
+        mux0 : mux port map(enable, tempout(i), Din(i), tempin(i));
         dffX : dff port map (clk, tempin(i), tempout(i));
     end generate GEN_dff;
 
     Dout <= tempout;
-
                                        
 end structural;
+
