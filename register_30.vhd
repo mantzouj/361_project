9c19ca5b4acb23af8d68015951a46086b908a25a
mantzouj <jasonmantzouranis2015@u.northwestern.edu>
2013-11-14 18:19:44 -0600

    Now includes new dffr_a

--- register_30.vhd
+++ register_30.vhd
@@ -11,30 +11,42 @@ entity register_30 is
       Din: in std_logic_vector(29 downto 0);
       clk: in std_logic;
       enable : in std_logic;
+      reset : in std_logic;
+      aload : in std_logic;
       Dout: out std_logic_vector(29 downto 0)
       );
   end register_30;
   
   architecture structural of register_30 is
   
-  component dff is
-      port (
-	     clk	: in  std_logic;
-	     d	: in  std_logic;
-	     q	: out std_logic
-      );
-  end component dff;
+  component dffr_a is
+    port (
+	   clk	   : in  std_logic;
+     arst   : in  std_logic;
+     aload  : in  std_logic;
+     adata  : in  std_logic;
+	   d	     : in  std_logic;
+     enable : in  std_logic;
+	   q	     : out std_logic
+  );
+  end component dffr_a;
     
   signal tempout : std_logic_vector(29 downto 0);
   signal tempin : std_logic_vector(29 downto 0);  
+  signal startaddress : std_logic_vector(29 downto 0);
     
   begin
+  
+  
+  startaddress <= "000000000100000000000000001000";
       
-  GEN_dff:
+      
+  GEN_dffr_a:
     for i in 0 to 29 generate
+      
       muxX : mux port map (enable, tempout(i), Din(i), tempin(i));    
-      dffX : dff port map (clk, tempin(i), tempout(i));
-  end generate GEN_dff;
+      dffr_aX : dffr_a port map (clk, reset, aload, startaddress(i), tempin(i), enable, tempout(i));
+  end generate GEN_dffr_a;
     
   Dout <= tempout;
 
